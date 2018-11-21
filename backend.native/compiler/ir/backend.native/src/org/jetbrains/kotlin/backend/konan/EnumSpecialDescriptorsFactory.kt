e369c4c26db462e6443b68d1933fff1c1116f8cd
Igor Chevdar <igor.chevdar@jetbrains.com>
2018-11-21 17:26:12 +0300

    qzz

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/EnumSpecialDescriptorsFactory.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/EnumSpecialDescriptorsFactory.kt
@@ -5,15 +5,24 @@
 
 package org.jetbrains.kotlin.backend.konan
 
+import org.jetbrains.kotlin.backend.common.descriptors.WrappedClassDescriptor
+import org.jetbrains.kotlin.backend.common.descriptors.WrappedFieldDescriptor
+import org.jetbrains.kotlin.backend.common.descriptors.WrappedSimpleFunctionDescriptor
 import org.jetbrains.kotlin.backend.common.ir.addSimpleDelegatingConstructor
+import org.jetbrains.kotlin.backend.common.ir.buildSimpleDelegatingConstructor
+import org.jetbrains.kotlin.backend.common.ir.ir2stringWhole
 import org.jetbrains.kotlin.backend.konan.descriptors.synthesizedName
 import org.jetbrains.kotlin.backend.konan.descriptors.enumEntries
+import org.jetbrains.kotlin.backend.konan.serialization.isExported
 import org.jetbrains.kotlin.descriptors.*
 import org.jetbrains.kotlin.descriptors.annotations.Annotations
 import org.jetbrains.kotlin.descriptors.impl.*
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.declarations.impl.*
 import org.jetbrains.kotlin.ir.symbols.IrSimpleFunctionSymbol
+import org.jetbrains.kotlin.ir.symbols.impl.IrClassSymbolImpl
+import org.jetbrains.kotlin.ir.symbols.impl.IrFieldSymbolImpl
+import org.jetbrains.kotlin.ir.symbols.impl.IrSimpleFunctionSymbolImpl
 import org.jetbrains.kotlin.ir.types.typeWith
 import org.jetbrains.kotlin.ir.util.*
 import org.jetbrains.kotlin.name.Name
@@ -23,14 +32,12 @@ import org.jetbrains.kotlin.storage.LockBasedStorageManager
 import org.jetbrains.kotlin.types.*
 
 
-internal object DECLARATION_ORIGIN_ENUM :
-        IrDeclarationOriginImpl("ENUM")
+internal object DECLARATION_ORIGIN_ENUM : IrDeclarationOriginImpl("ENUM")
 
 internal data class LoweredEnum(val implObject: IrClass,
                                 val valuesField: IrField,
                                 val valuesGetter: IrSimpleFunction,
                                 val itemGetterSymbol: IrSimpleFunctionSymbol,
-                                val itemGetterDescriptor: FunctionDescriptor,
                                 val entriesMap: Map<Name, Int>)
 
 internal class EnumSpecialDeclarationsFactory(val context: Context) {
@@ -42,48 +49,109 @@ internal class EnumSpecialDeclarationsFactory(val context: Context) {
 
         val implObjectDescriptor = ClassDescriptorImpl(enumClassDescriptor, "OBJECT".synthesizedName, Modality.FINAL,
                 ClassKind.OBJECT, listOf(context.builtIns.anyType), SourceElement.NO_SOURCE, false, LockBasedStorageManager.NO_LOCKS)
-
-        val implObject = IrClassImpl(startOffset, endOffset, DECLARATION_ORIGIN_ENUM, implObjectDescriptor).apply {
-            createParameterDeclarations()
+//
+//        val implObject = IrClassImpl(startOffset, endOffset, DECLARATION_ORIGIN_ENUM, implObjectDescriptor).apply {
+//            createParameterDeclarations()
+//        }
+        val implObject = WrappedClassDescriptor().let {
+            IrClassImpl(
+                    startOffset = startOffset,
+                    endOffset = endOffset,
+                    origin = DECLARATION_ORIGIN_ENUM,
+                    symbol = IrClassSymbolImpl(it),
+                    name = "OBJECT".synthesizedName,
+                    kind = ClassKind.OBJECT,
+                    visibility = Visibilities.PUBLIC,
+                    modality = Modality.FINAL,
+                    isCompanion = false,
+                    isInner = false,
+                    isData = false,
+                    isExternal = false,
+                    isInline = false
+            ).apply {
+                it.bind(this)
+                parent = enumClass
+                createParameterDeclarations()
+            }
         }
 
-        val valuesProperty = createEnumValuesField(enumClassDescriptor, implObjectDescriptor)
         val valuesType = context.ir.symbols.array.typeWith(enumClass.defaultType)
-        val valuesField = IrFieldImpl(startOffset, endOffset, DECLARATION_ORIGIN_ENUM, valuesProperty, valuesType)
-
-        val valuesGetterDescriptor = createValuesGetterDescriptor(enumClassDescriptor, implObjectDescriptor)
-        val valuesGetter = IrFunctionImpl(startOffset, endOffset, DECLARATION_ORIGIN_ENUM, valuesGetterDescriptor).also {
-            it.returnType = valuesType
-            it.parent = implObject
-            //it.createDispatchReceiverParameter()
+//        val valuesProperty = createEnumValuesField(enumClassDescriptor, implObjectDescriptor)
+//        val valuesField = IrFieldImpl(startOffset, endOffset, DECLARATION_ORIGIN_ENUM, valuesProperty, valuesType)
+        val valuesField = WrappedFieldDescriptor().let {
+            IrFieldImpl(
+                    startOffset,
+                    endOffset,
+                    DECLARATION_ORIGIN_ENUM,
+                    IrFieldSymbolImpl(it),
+                    "VALUES".synthesizedName,
+                    valuesType,
+                    Visibilities.PRIVATE,
+                    true,
+                    false,
+                    false
+            ).apply {
+                it.bind(this)
+                parent = implObject
+            }
         }
 
-        val memberScope = MemberScope.Empty
+        val valuesGetter = WrappedSimpleFunctionDescriptor().let {
+            IrFunctionImpl(
+                    startOffset,
+                    endOffset,
+                    DECLARATION_ORIGIN_ENUM,
+                    IrSimpleFunctionSymbolImpl(it),
+                    "get-VALUES".synthesizedName,
+                    Visibilities.PUBLIC,
+                    Modality.FINAL,
+                    false,
+                    false,
+                    false,
+                    false
+            ).apply {
+                it.bind(this)
+                parent = implObject
+                returnType = valuesType
+            }
+        }
+        val valuesGetterDescriptor = createValuesGetterDescriptor(enumClassDescriptor, implObjectDescriptor)
+
+        if (valuesGetterDescriptor.isExported() != valuesGetter.descriptor.isExported()) {
+            println("BUGBUGBUG: ${valuesGetterDescriptor} ${ir2stringWhole(valuesGetter)}")
+        }
+
+//        val valuesGetter = IrFunctionImpl(startOffset, endOffset, DECLARATION_ORIGIN_ENUM, valuesGetterDescriptor).also {
+//            it.returnType = valuesType
+//            it.parent = implObject
+//        }
 
         val constructorOfAny = context.irBuiltIns.anyClass.owner.constructors.first()
         // TODO: why primary?
-        val constructor = implObject.addSimpleDelegatingConstructor(
+//        val constructor = implObject.addSimpleDelegatingConstructor(
+//                constructorOfAny,
+//                context.irBuiltIns,
+//                DECLARATION_ORIGIN_ENUM,
+//                true
+//        )
+        implObject.buildSimpleDelegatingConstructor(
                 constructorOfAny,
                 context.irBuiltIns,
-                DECLARATION_ORIGIN_ENUM,
                 true
         )
-        val constructorDescriptor = constructor.descriptor
 
-        implObjectDescriptor.initialize(memberScope, setOf(constructorDescriptor), constructorDescriptor)
         implObject.superTypes += context.irBuiltIns.anyType
         implObject.addFakeOverrides()
-        implObject.parent = enumClass
-        valuesField.parent = implObject
 
-        val (itemGetterSymbol, itemGetterDescriptor) = getEnumItemGetter(enumClassDescriptor)
+        //val (itemGetterSymbol, itemGetterDescriptor) = getEnumItemGetter(enumClassDescriptor)
 
         return LoweredEnum(
                 implObject,
                 valuesField,
                 valuesGetter,
-                itemGetterSymbol, itemGetterDescriptor,
-                createEnumEntriesMap(enumClassDescriptor))
+                //itemGetterSymbol,BinaryInterface.kt
+                context.ir.symbols.array.functions.single { it.descriptor.name == Name.identifier("get") },
+                createEnumEntriesMap(enumClass))
     }
 
     private fun createValuesGetterDescriptor(enumClassDescriptor: ClassDescriptor, implObjectDescriptor: ClassDescriptor)
@@ -133,12 +201,19 @@ internal class EnumSpecialDeclarationsFactory(val context: Context) {
         return getter to getter.descriptor.substitute(typeSubstitutor)!!
     }
 
-    private fun createEnumEntriesMap(enumClassDescriptor: ClassDescriptor): Map<Name, Int> {
-        val map = mutableMapOf<Name, Int>()
-        enumClassDescriptor.enumEntries
-                .sortedBy { it.name }
-                .forEachIndexed { index, entry -> map.put(entry.name, index) }
-        return map
-    }
+//    private fun createEnumEntriesMap(enumClassDescriptor: ClassDescriptor): Map<Name, Int> {
+//        val map = mutableMapOf<Name, Int>()
+//        enumClassDescriptor.enumEntries
+//                .sortedBy { it.name }
+//                .forEachIndexed { index, entry -> map.put(entry.name, index) }
+//        return map
+//    }
 
+    private fun createEnumEntriesMap(enumClass: IrClass) =
+            enumClass.declarations
+                    .filterIsInstance<IrEnumEntry>()
+                    .sortedBy { it.name }
+                    .withIndex()
+                    .associate { it.value.name to it.index }
+                    .toMap()
 }
